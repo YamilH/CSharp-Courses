ae696d5d9b3d861fea215cc10a0e7f6345de812e
YamilH <yamilhamui@gmail.com>
2023-09-02 16:06:31 -0300

    to repo

---
+++ Curso 2/POO1/ejemplo1/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Persona
+    {
+        // Persona: edad, sueldo, nombre
+        // ATRIBUTOS, O DATOS MIEMBRO DE LA CLASE,
+        // variable que permite guardar la informacion
+
+        private int edad;
+        private float sueldo;
+        private string nombre;
+
+        //con METODOS
+
+        public void setEdad(int e)
+        {
+            edad = e;
+        }
+
+        public int getEdad()
+        {
+            return edad; 
+        }
+
+        
+    
+    
+    }
+}
---
+++ Curso 2/POO1/ejemplo1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Clases, Objetos y Propiedades. Encapsulamiento.
+       
+            Persona p1 = new Persona();
+            p1.setEdad(20);
+
+            Console.WriteLine("la edad de la persona es: " + p1.getEdad());
+
+            Botella b1 = new Botella();
+            b1.Capacidad = 200;
+
+            int algo = b1.Capacidad;
+
+
+            Perro pe1 = new Perro();
+            pe1.Nombre = "Tito";
+            string something = pe1.Nombre;
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
