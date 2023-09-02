ae74563685a5f380be99d034b1618a593536ef3c
YamilH <yamilhamui@gmail.com>
2023-09-02 10:31:34 -0300

    to repo

---
+++ Curso 1/8. Funciones/Ejercicio-8-5/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Ejercicio_8_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("nro1");
+            // int a = int.Parse(Console.ReadLine());
+            // Console.WriteLine("nro2");
+            // int b = int.Parse(Console.ReadLine());
+
+            // Console.WriteLine("El resultado es: " + haceAlgo2(ref a, ref b));
+            // Console.WriteLine("El resultado es: " + haceAlgo2(ref a, ref b));
+            // Console.WriteLine("El resultado es: " + haceAlgo2(ref a, ref b));
+
+
+        int n;
+        int suma;
+
+        for (int x = 0; x < 3; x++)
+        {
+            n = int.Parse(Console.ReadLine());
+            suma = 0;
+
+            while (n != 0)
+            {
+                suma += n + 5;
+                n = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma de la vuelta " + (x + 1) + " fue de: " + suma);
+        }
+
+
+        
+
+        // static int haceAlgo2(ref int numero, ref int numero2)
+        // {
+        //     int res;
+        //     numero  = numero * 2;
+        //     numero2 = numero2 * 3;
+
+        //     res = numero * numero2;
+
+        //     return res;
+        // }
+
+
+    }}
+}
---
+++ Curso 2/PrimerosEjemplos/ejemplos2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemplos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola desde .net");
+            Console.ReadKey();
+        }
+    }
+}
