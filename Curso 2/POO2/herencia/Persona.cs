d02ca4a09bd15c5d9a150e2da14e482cbf49731e
YamilH <yamilhamui@gmail.com>
2023-09-15 11:15:00 -0300

    to repo

---
+++ Curso 2/POO2/herencia/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia
+{
+    internal class Persona
+    {
+
+        // Persona sería mi clase base
+        public string Nombre { get; set; }
+        
+        public string Apellido { get; set; }
+
+        public int Legajo { get; set; }
+
+
+    }
+}
---
+++ Curso 2/POO2/herencia2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehiculo v1 = new Vehiculo();
+            Camioneta c1 = new Camioneta();
+
+            Vehiculo v2 = new Camioneta();
+        }
+    }
+}
