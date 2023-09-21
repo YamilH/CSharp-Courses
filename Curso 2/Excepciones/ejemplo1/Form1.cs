0a413e0a659bec228953870903e14f778c0d23ca
YamilH <yamilhamui@gmail.com>
2023-09-21 10:53:42 -0300

    to repo

--- Curso 2/Excepciones/ejemplo1/Form1.cs
+++ Curso 2/Excepciones/ejemplo1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,15 +20,22 @@ namespace ejemplo1
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            int a, b, r;
+            //int a, b, r;
+            //try
+            //{
+            //    a = int.Parse(txt1.Text);
+            //    b = int.Parse(txt2.Text);
+            //    r = a / b;
+            //    lblResultado.Text = "= " + r;
+            //}
+
+            int resultado;
             try
             {
-                a = int.Parse(txt1.Text);
-                b = int.Parse(txt2.Text);
-                r = a / b;
-                lblResultado.Text = "= " + r;
+                resultado = calcular();
+                lblResultado.Text = "= " + resultado;
             }
-            catch ( FormatException ex )
+            catch (FormatException ex)
             {
                 MessageBox.Show("Por favor cargar solo números..");
             }
@@ -38,6 +46,29 @@ namespace ejemplo1
             catch (Exception ex)
             {
                 MessageBox.Show("Error no reconocido.. Contactar a tu dev..");
+            }
+            finally
+            {
+                // instrucciones que se van a ejecutar si o si
+                // operaciones sensibles
+            }
+
+
+        }
+
+        private int calcular()
+        {
+            int a, b, r;
+            try
+            {
+                a = int.Parse(txt1.Text);
+                b = int.Parse(txt2.Text);
+                r = a / b;
+                return r;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
---
+++ Curso 2/ejemplo-ado-net/winform-app/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winform_app
+{
+    public partial class Form1 : Form
+    {
+        private List<Pokemon> listaPokemon;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            listaPokemon = negocio.listar();
+            dgvPokemons.DataSource = listaPokemon;
+            dgvPokemons.Columns["UrlImagen"].Visible = false;
+            cargarImagen(listaPokemon[0].UrlImagen);
+
+        }
+
+        private void dgvPokemons_SelectionChanged(object sender, EventArgs e)
+        {
+            Pokemon seleccionado = (Pokemon)dgvPokemons.CurrentRow.DataBoundItem;
+            cargarImagen(seleccionado.UrlImagen);
+
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pbPokemon.Load("https://thinkfirstcommunication.com/wp-content/uploads/2022/05/placeholder-1-1.png");
+            }
+        }
+    }
+}
